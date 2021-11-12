6daa8ac083c1d3902d0cbf9a7f410bcb545e8cae
wazimuhb <hesbonosoro1@gmail.com>
2021-11-12 18:52:10 +0300

    Google Analytics and Heroku HTTP Link

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -6,7 +6,7 @@ import { createHttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
-const httpLink = createHttpLink({ uri: 'http://localhost:5000' });
+const httpLink = createHttpLink({ uri: 'https://desolate-atoll-91043.herokuapp.com/' || 'http://localhost:5000' });
 const authLink = setContext(() => {
 	const token = localStorage.getItem('jwtToken');
 	return { headers: { Authorization: token ? `Bearer ${token}` : '' } };

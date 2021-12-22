ba17ba2057ffd29af6bf5954f5895a098991cdfd
wazimuhb <hesbonosoro1@gmail.com>
2021-12-22 20:43:52 +0300

    prettier format

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,6 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({ uri: 'https://chat-app-merng-server.herokuapp.com/' || 'http://localhost:5000' });
+
 const authLink = setContext(() => {
 	const token = localStorage.getItem('jwtToken');
 	return { headers: { Authorization: token ? `Bearer ${token}` : '' } };
